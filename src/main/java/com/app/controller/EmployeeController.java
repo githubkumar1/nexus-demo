915879fc4e7d077dce9dcbee290ba994036dc269
raviyasas <raviyasas@live.com>
2020-03-21 10:02:21 +0530

    Initial commit

---
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.app.controller;
+
+import com.app.entity.Employee;
+import com.app.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService=employeeService;
+    }
+
+    private EmployeeService employeeService;
+
+    @GetMapping("/getEmployees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getEmployees();
+    }
+}
